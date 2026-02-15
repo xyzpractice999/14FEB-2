b2b386603a4dff7d3a70fd6ea643e130be0650dc
kishore <skill2automate@gmail.com>
2026-02-15 09:31:25 +0530

    changes done

--- tests/example5.spec.js
+++ tests/example5.spec.js
@@ -13,8 +13,7 @@ test('get started link', async ({ page }) => {
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
-  await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
