f09eef66a3daaabf6233c23d6e638ed1bce494f1
xyzpractice999 <skill2automate@gmail.com>
2026-02-15 09:34:29 +0530

    Add test for 'get started' link functionality

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -18,3 +18,14 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('get started link 33', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
